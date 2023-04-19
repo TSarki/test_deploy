3b049a540b06b34788b48f79992127c117899e41
Artem Sargsyan <sarkiman@icloud.com>
2023-04-19 16:02:19 +0200

    fix test

--- tests/measurement/test.py
+++ tests/measurement/test.py
@@ -3,8 +3,4 @@ from rest_framework.test import APIClient
 
 class TestSampleView(TestCase):
     def test_ci_cd(self):
-        url = '/api/sensors/'
-        client = APIClient()
-        response = client.get(url)
-        print(response)
         self.assertEqual(2, 2)
